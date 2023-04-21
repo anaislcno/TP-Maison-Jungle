c0c35b8c59cc422ae75d64888aa93bb7e0e78ac7
An <anaislcno@gmail.com>
2023-04-21 14:18:48 +0200

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleInput(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleBlur() {
+    if (!inputValue.includes("@")) {
+      alert("Attention, l'adresse mail n'est pas valide");
+    }
+  }
+
+  return (
+    <footer className="lmj-footer">
+      <div className="lmj-footer-elem">
+        Pour les passionné·e·s de plantes 🌿🌱🌵
+      </div>
+      <div className="lmj-footer-elem">Laissez-nous votre mail :</div>
+      <input
+        placeholder="Entrez votre mail"
+        onChange={handleInput}
+        value={inputValue}
+        onBlur={handleBlur}
+      />
+    </footer>
+  );
+}
+
+export default Footer;
