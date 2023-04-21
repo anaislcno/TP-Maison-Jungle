a663d3fc86b1f0725d93bd59c5fe04ebb4f63956
An <anaislcno@gmail.com>
2023-04-21 14:30:48 +0200

    add the basket

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,11 +1,18 @@
 import monstera from "../assets/monstera.jpeg";
+import lyrata from "../assets/lyrata.jpeg";
+import pothos from "../assets/pothos.jpeg";
+import succulent from "../assets/succulent.jpeg";
+import olivier from "../assets/olivier.jpeg";
+import basil from "../assets/basil.jpeg";
+import mint from "../assets/mint.jpeg";
+import calathea from "../assets/calathea.jpeg";
+import cactus from "../assets/cactus.jpeg";
 
 export const plantList = [
   {
     name: "monstera",
     category: "classique",
     id: "1ed",
-    isBestSale: true,
     light: 2,
     water: 3,
     cover: monstera,
@@ -16,23 +23,24 @@ export const plantList = [
     id: "2ab",
     light: 3,
     water: 1,
-    cover: monstera,
+    cover: lyrata,
   },
+
   {
     name: "pothos argenté",
     category: "classique",
     id: "3sd",
     light: 1,
     water: 2,
-    cover: monstera,
+    cover: pothos,
   },
   {
-    name: "yucca",
+    name: "calathea",
     category: "classique",
     id: "4kk",
-    light: 3,
-    water: 1,
-    cover: monstera,
+    light: 2,
+    water: 3,
+    cover: calathea,
   },
   {
     name: "olivier",
@@ -40,32 +48,24 @@ export const plantList = [
     id: "5pl",
     light: 3,
     water: 1,
-    cover: monstera,
+    cover: olivier,
   },
+
   {
-    name: "géranium",
-    category: "extérieur",
-    id: "6uo",
+    name: "cactus",
+    category: "plante grasse",
+    id: "8fp",
     light: 2,
-    water: 2,
-    cover: monstera,
+    water: 1,
+    cover: cactus,
   },
   {
     name: "basilique",
     category: "extérieur",
     id: "7ie",
-    isBestSale: true,
     light: 2,
     water: 3,
-    cover: monstera,
-  },
-  {
-    name: "aloe",
-    category: "plante grasse",
-    id: "8fp",
-    light: 2,
-    water: 1,
-    cover: monstera,
+    cover: basil,
   },
   {
     name: "succulente",
@@ -73,6 +73,15 @@ export const plantList = [
     id: "9vn",
     light: 2,
     water: 1,
-    cover: monstera,
+    cover: succulent,
+  },
+
+  {
+    name: "menthe",
+    category: "extérieur",
+    id: "6uo",
+    light: 2,
+    water: 2,
+    cover: mint,
   },
 ];
