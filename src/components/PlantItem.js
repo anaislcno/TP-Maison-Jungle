ccc40992e1c240d9515bb8c3e085a60714e48fd7
An <anaislcno@gmail.com>
2023-04-27 12:16:03 +0200

    fix modal for each plant card

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import Modal from "react-modal";
 import CareScale from "./CareScale";
 import "../styles/PlantItem.css";
-import Modal from "react-modal";
+import ReactModal from "react-modal";
 
 // function handleClick(plantName) {
 //   alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`);
@@ -12,42 +13,44 @@ function PlantItem({ id, cover, name, water, light, price }) {
 
   function openModal() {
     setModalIsOpen(true);
-  }
-
-  function closeModal() {
-    setModalIsOpen(false);
+    ReactModal.defaultStyles.content = null;
+    ReactModal.defaultStyles.overlay = null;
   }
 
   return (
-    <>
-      <li key={id} className="lmj-plant-item">
-        <span className="lmj-plant-item-price">{price}€</span>
-        <img
-          className="lmj-plant-item-cover"
-          src={cover}
-          alt={`${name} cover`}
-          onClick={openModal} // Ouvre la modale lorsque l'image est cliquée
-        />
-        {name}
-        <div>
-          <CareScale careType="water" scaleValue={water} />
-          <CareScale careType="light" scaleValue={light} />
+    <li key={id} className="lmj-plant-item">
+      <span className="lmj-plant-item-price">{price}€</span>
+      <img
+        className="lmj-plant-item-cover"
+        src={cover}
+        alt={`${name} cover`}
+        onClick={openModal}
+      />
+      {name}
+      <div>
+        <CareScale careType="water" scaleValue={water} />
+        <CareScale careType="light" scaleValue={light} />
+      </div>
+      <Modal isOpen={modalIsOpen}>
+        <div className="modal">
+          <div className="overlay"></div>
+          <div className="modal-content">
+            <h2>Informations complémentaires sur {name}</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              perferendis suscipit officia recusandae, eveniet quaerat assumenda
+              id fugit, dignissimos maxime non natus placeat illo iusto!
+              Sapiente dolorum id maiores dolores? Illum pariatur possimus
+              quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
+              placeat tempora vitae enim incidunt porro fuga ea.
+            </p>
+            <button className="btn-modal" onClick={() => setModalIsOpen(false)}>
+              Fermer
+            </button>
+          </div>
         </div>
-        <button className="">Ajouter au panier</button>
-      </li>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-        <h2>Hello Modal</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-          perferendis suscipit officia recusandae, eveniet quaerat assumenda id
-          fugit, dignissimos maxime non natus placeat illo iusto! Sapiente
-          dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos
-          molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae
-          enim incidunt porro fuga ea.
-        </p>
-        <button onClick={closeModal}>CLOSE</button>
       </Modal>
-    </>
+    </li>
   );
 }
 
